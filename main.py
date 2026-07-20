9959ebdc584967e6f02406f70f5413d02e2daf58
Raksha Manoj Singh <singhhrakshaa28@gmail.com>
2026-07-20 20:32:21 +0530

    added comments, corrected the code

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import HTMLResponse
-import os
-import cv2
-import numpy as np
+from fastapi import FastAPI, File, UploadFile, HTTPException 
+from fastapi.responses import HTMLResponse #return HTML content to the browser instead of JSON
+import os #to work with files and folders
+import cv2 #decode the img
+import numpy as np #convert raw bytes into numpy array
 from ocr_utils import extract_text_from_image
 
-app = FastAPI()
+app = FastAPI() #starts your FastAPI application
 
-root = os.path.dirname(os.path.abspath(__file__))
+root = os.path.dirname(os.path.abspath(__file__)) #find the html file
 static_dir = os.path.join(root, "static")
 index_path = os.path.join(static_dir, "index.html")
 
-@app.get("/")
-async def index():
-    with open(index_path, "r", encoding="utf-8") as f:
+@app.get("/") #opens page
+async def index(): 
+    with open(index_path, "r", encoding="utf-8") as f: #opens file in read mode
         html = f.read()
     return HTMLResponse(content=html, status_code=200)
 
 @app.post("/ocr")
 async def ocr_endpoint(file: UploadFile = File(...)):
-    try:
+    try: #error handling
         contents = await file.read()
-        img_arr = np.frombuffer(contents, np.uint8)
+        img_arr = np.frombuffer(contents, np.uint8) #bytes to array
         img = cv2.imdecode(img_arr, cv2.IMREAD_COLOR)
 
         if img is None:
-            raise HTTPException(status_code=400, detail="Invalid image")
+            raise HTTPException(status_code=400, detail="Invalid image") #checks if decoding worked
 
         result = extract_text_from_image(img)
 
